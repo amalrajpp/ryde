e81e2e9367fb0cf1a0c08a222018f595e45be7cf
Amal Raj <amal@gmail.com>
2025-11-18 18:47:40 +0530

    location

--- ryde/lib/main.dart
+++ ryde/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ryde/features/screen/on_borading_screen.dart';
 import 'package:ryde/features/screen/redirect.dart';
 import 'package:ryde/firebase_options.dart';
+import 'package:ryde/services/location_permission.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  requestLocationPermission();
   runApp(const MyApp());
 }
 
